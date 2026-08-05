cc378f7ba0ff11542bbdc71d60f8984cc48b3a7e
Ling Song <lingsong.c4@gmail.com>
2026-08-05 12:14:13 +0800

    added thumbnail

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/MurkyYT/cs2-map-icons/main/images/thumbs/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
